d1fe3f50d724be744846fc5e495a89deed45c97e
Sajarin M <sajarinm@yahoo.com>
2021-06-03 20:42:29 +0100

    fix syntax error

--- models/result.js
+++ models/result.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 const resultSchema = new Schema({
-	student: {type: Schema.Types.ObjectId,ref: "User",}
+	student: {type: Schema.Types.ObjectId,ref: "User",},
 	exam: {type: Schema.Types.ObjectId,ref: "Exam",},
 	startingTime: {type: Date,default: Date.now(),},
 	answers: {type: [String],},

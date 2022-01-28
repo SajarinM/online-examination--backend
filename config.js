1c66bb3d01fa94d6a3693f9f2b88770a30c4c306
Sajarin M <sajarinm@yahoo.com>
2022-01-28 14:16:12 +0530

    change env name for heroku

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 const jwtPrivateKey = process.env.jwtPrivateKey || "secret";
 const dbUrl = process.env.dbUrl || "mongodb://localhost:27017/test";
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 module.exports = {
 	jwtPrivateKey,
